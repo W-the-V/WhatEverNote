d90b5db58b3468c0dc85219867a28cb06d33cf51
OByrnes <liviabyrnes@gmail.com>
2021-03-20 20:31:58 -0400

    Home page

--- react-app/src/components/NavBar/index.js
+++ react-app/src/components/NavBar/index.js
@@ -7,15 +7,15 @@ const NavBar = ({ setAuthenticated }) => {
   let user = {"id":1,"firstname":"Fake", "lastname": "McDonald", "username":"FakeMcFake", "email":"fake@fake.com"}
   return (
     <nav>
-      <div class="nav_top__circles">
-        <div class="nav_circles letter-circle">
+      <div className="nav_top__circles">
+        <div className="nav_circles letter-circle">
           {user.firstname[0]}
         </div>
-        <div class="nav_circles search-circle">
-          <i class="fas fa-search"></i>
+        <div className="nav_circles search-circle">
+          <i className="fas fa-search"></i>
         </div>
-        <div class="nav_circles plus-circle">
-          <i class="fas fa-plus"></i>
+        <div className="nav_circles plus-circle">
+          <i className="fas fa-plus"></i>
         </div>
 
       </div>
@@ -23,31 +23,31 @@ const NavBar = ({ setAuthenticated }) => {
         <div className="middle_nav__container">
           <div className="icon__holder home_icon">
             <NavLink to="/" exact={true} activeClassName="active">
-              <i class="fas fa-home"></i>
+              <i className="fas fa-home"></i>
             </NavLink>
           </div>
           <div className="icon__holder star_icon">
-            <i class="fas fa-star"></i>
+            <i className="fas fa-star"></i>
 
           </div>
           <div className="icon__holder note_icon">
-            <i class="fas fa-file-alt"></i>
+            <i className="fas fa-file-alt"></i>
           </div>
         </div>
         <div className="middle2_nav__container">
         <div className="icon__holder notebook_icon">
-          <i class="fas fa-book"></i>
+          <i className="fas fa-book"></i>
         </div>
         <div className="icon__holder tag_icon">
-          <i class="fas fa-tag"></i>
+          <i className="fas fa-tag"></i>
         </div>
         <div className="icon__holder user_icon">
-          <i class="fas fa-user-friends"></i>
+          <i className="fas fa-user-friends"></i>
         </div>
         </div>
         <div className="middle3_nav__container">
         <div className="icon__holder trash_icon">
-          <i class="fas fa-trash"></i> 
+          <i className="fas fa-trash"></i> 
         </div>
         </div>
       </div>
