66c8e7067e6f47669550d6ec6bda3465a65231a5
Alpesh <alpesh.vyas627@gmail.com>
2021-03-25 07:22:47 -0700

    all notes routes work and have been tested but validations needed

--- app/api/note_routes.py
+++ app/api/note_routes.py
@@ -7,21 +7,15 @@ from app.models import User, Note, Notebook, db
 
 note_routes = Blueprint('note_routes', __name__)
 
+def get_one_note(note_id):
+    note = Note.query.filter_by(id = note_id).first()
+    return note
+
 def get_all_notes(user_id):
-    
     notebooks = Notebook.query.filter_by(userId = user_id).all()
-   
     return jsonify({"notebooks": [notebook.to_dict() for notebook in notebooks]})
 
-def delete_note(note_id):
-    Note.query.filter_by(id = note_id).delete()
-
-    # session.delete(note)
-    db.session.commit()
-    return "success!"
-
 def add_one_note(id):
-
     note_data = json.loads(request.data.decode("utf-8"))
 
     note = Note(title = note_data["title"],
@@ -30,8 +24,29 @@ def add_one_note(id):
     
     db.session.add(note)
     db.session.commit()
-
     return jsonify(note.to_dict())
+
+def delete_note(note_id):
+    Note.query.filter_by(id = note_id).delete()
+
+    db.session.commit()
+    return "success!"
+
+def edit_note(note_id):
+    edit_note_data = json.loads(request.data.decode("utf-8"))
+    note = get_one_note(note_id)
+
+    if note.title is not edit_note_data["title"]:
+        note.title = edit_note_data["title"]
+    if note.text is not edit_note_data["text"]:
+        note.text = edit_note_data["text"]
+    if note.notebook_id is not edit_note_data["notebook_id"]:
+        note.notebook_id = edit_note_data["notebook_id"]
+    
+    db.session.commit()
+    return jsonify(note.to_dict())
+
+
 
 #get_all_notes works--tested
 #add_one_note works--tested --need to add validations, etc
@@ -42,10 +57,16 @@ def get_or_add_notes(id):
     elif request.method == 'POST':
         return add_one_note(id)
 
-#delete note works--tested
+#delete note works--tested --need to add validations
 @note_routes.route("/api/user/<int:user_id>/notes/<int:note_id>", methods = ['DELETE'])
 def delete_user_note(user_id, note_id):
     return delete_note(note_id)
+
+#edit_note works--tested --need to add validations
+@note_routes.route("/api/user/<int:id>/notes/<int:note_id>", methods=['PUT'])
+def edit_user_note(id, note_id):
+    return edit_note(note_id)
+
 
 # def get_notes(id):
     # print(request.base_url)
