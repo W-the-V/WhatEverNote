915eef4211fc938f1b8238b28397a4a040550e61
walt <walter.hills5@gmail.com>
2021-04-28 19:21:32 -0500

    notes correctly updating, misc fixes

--- react-app/src/components/NoteHeader/index.js
+++ react-app/src/components/NoteHeader/index.js
@@ -1,60 +1,80 @@
-import React, { useState } from 'react';
-import Dropdown from '../Dropdown';
-import './NoteHeader.css';
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import Dropdown from "../Dropdown";
+import "./NoteHeader.css";
 
-function NoteHeader() {
-  const [notebookName, setNotebookName ] = useState('Notebook');
+function NoteHeader({ selectedNote }) {
+  const [notebookName, setNotebookName] = useState("Notebook");
   const [expand, setExpand] = useState(false);
-  const [expandStyle, setExpandStyle] = useState("flex: 1 0 10%")
+  const [expandStyle, setExpandStyle] = useState("flex: 1 0 10%");
+  let notebooks = useSelector((state) => state.notebooks.notebooks);
 
+  const findInfo = (notebooks, selected) => {
+    if (notebooks) {
+      let current = notebooks.filter(
+        (notebook) => notebook.id === selected.notebook_id
+      );
+      console.log(current);
+      return (
+        <>
+          <div className="notebook-label">
+            <button type="button" id="notebook-button">
+              <span id="notebook-icon-container">
+                <i className="fas fa-book"></i>
+              </span>
+              <span id="notebook-name">{current[0].name}</span>
+            </button>
+            <div></div>
+          </div>
+          <div className="divider">
+            <i class="fas fa-grip-lines-vertical"></i>
+          </div>
+          <button type="button" id="notebook-button">
+            <span id="notebook-icon-container">
+              <i className="fas fa-file-alt"></i>
+            </span>
+            <span id="notebook-name">{selected.title}</span>
+          </button>
+        </>
+      );
+    }
+  };
   const expandOnClick = () => {
     setExpand(!expand);
-  }
+  };
   return (
-		<>
-			<div className='header' id='note-header'>
-				<div className='left-container container'>
-					<button className='button' id='fullscreen-button' type='button' onClick={expandOnClick}>
+    <>
+      <div className="header" id="note-header">
+        <div className="left-container container">
+          {/* <button className='button' id='fullscreen-button' type='button' onClick={expandOnClick}>
 						<div>
 							<i class='fas fa-expand'></i>
 						</div>
-					</button>
-					<div className='divider'>
-						<i class='fas fa-grip-lines-vertical'></i>
-					</div>
-					<div className='notebook-label'>
-						<button type='button' id='notebook-button'>
-							<span id='notebook-icon-container'>
-								<i className='fas fa-book'></i>
-							</span>
-							<span id='notebook-name'>Notebook</span>
-						</button>
-						<div></div>
-					</div>
-				</div>
-
-				<div className='right-container container'>
-					<div className='who-can-see'>Only You</div>
-					<span>
-						<button className='share-button'>Share</button>
-					</span>
-					<Dropdown
-						items={[
-							"Share...",
-							"Move...",
-							"Copy To...",
-							"Duplicate",
-							"",
-							"Edit tags",
-							"",
-							"Add to Shortcuts",
-							"Copy internal link",
-							"Find within note",
-						]}
-					/>
-				</div>
-			</div>
-		</>
+					</button> */}
+          {selectedNote && findInfo(notebooks, selectedNote)}
+        </div>
+        <div className="right-container container">
+          <div className="who-can-see">Only You</div>
+          <span>
+            <button className="share-button">Share</button>
+          </span>
+          <Dropdown
+            items={[
+              "Share...",
+              "Move...",
+              "Copy To...",
+              "Duplicate",
+              "",
+              "Edit tags",
+              "",
+              "Add to Shortcuts",
+              "Copy internal link",
+              "Find within note",
+            ]}
+          />
+        </div>
+      </div>
+    </>
   );
 }
 
--- react-app/src/components/NotePage/NotePage.js
+++ react-app/src/components/NotePage/NotePage.js
@@ -1,137 +1,150 @@
-import React, { useEffect, useState } from 'react'
-import { useSelector,useDispatch } from 'react-redux'
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { NavLink, Redirect, useLocation } from "react-router-dom";
-import {getNotes} from "../../store/notes"
-import "./index.css"
-import Note from "./note"
-import NoteInList from "./NoteInList"
-import NoteHeader from "../NoteHeader/index"
-import { useHistory, useParams } from 'react-router'
-import { getNotebooks } from '../../store/notebooks'
-import NoteDropDown from './NoteDropDown';
-import { useSelectedNote } from '../../context/NoteContext';
+import { getNotes } from "../../store/notes";
+import "./index.css";
+import Note from "./note";
+import NoteInList from "./NoteInList";
+import NoteHeader from "../NoteHeader/index";
+import { useHistory, useParams } from "react-router";
+import { getNotebooks } from "../../store/notebooks";
+import NoteDropDown from "./NoteDropDown";
+import { useSelectedNote } from "../../context/NoteContext";
 const NotePage = () => {
+  let user = useSelector((state) => state.session.user);
+  let notes = useSelector((state) => state.notes?.notes);
+  const history = useHistory();
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getNotes(user.id));
+    dispatch(getNotebooks(user.id));
+  }, [dispatch]);
 
+  let sortCriteriaList = ["updatedAt", "title"];
+  const [ascending, setAscending] = useState(false);
+  const { selectedNote, setSelectedNote } = useSelectedNote();
+  const [sortCriteriaDD, setSortCriteriaDD] = useState(false);
+  const [sortCriteria, setSortCriteria] = useState(sortCriteriaList[0]);
+  let urlPath = useLocation();
+  let path = urlPath.pathname.split("/");
+  let noteSelected;
 
-    const history = useHistory()
-    const dispatch = useDispatch()
-    useEffect(()=>{
-        dispatch(getNotes(user.id))
-        dispatch(getNotebooks(user.id))
-    }, [dispatch])
-    let user = useSelector(state => state.session.user)
-    let notes = useSelector(state => state.notes?.notes)
-    let sortCriteriaList = ["updatedAt", "title"]
-    const [ascending, setAscending] = useState(false)
-    const {selectedNote, setSelectedNote} = useSelectedNote()
-    const [sortCriteriaDD, setSortCriteriaDD] = useState(false)
-    const [sortCriteria, setSortCriteria] = useState(sortCriteriaList[0])
-    let urlPath = useLocation()
-    let path = urlPath.pathname.split("/")
-    let noteSelected;
-    
-    
-    
-    useEffect(()=>{
-        
-        if(notes){
-        setSelectedNote("")
-        if(ascending && sortCriteria === "updatedAt"){
-                notes.sort((a, b) => {
-                 return new Date(a.updated_at) - new Date(b.updated_at)
-                    })
-    
-                }
-        else if (ascending && sortCriteria === "title"){
-            notes.sort((a,b) => {
-                let aTitle = a.title.toUpperCase()
-                let bTitle = b.title.toUpperCase()
-                if (aTitle < bTitle){
-                    return -1
-                }
-                if (bTitle < aTitle){
-                    return 1
-                }
-                return 0
-            })
-            
-        }
-        else if (!ascending && sortCriteria === "updatedAt"){
-            notes.sort((a, b) => {
-                return new Date(b.updated_at) - new Date(a.updated_at)
-                   })
-                   
-        }
-        else if (!ascending && sortCriteria === "title"){
-            notes.sort((a,b) => {
-                let aTitle = a.title.toUpperCase()
-                let bTitle = b.title.toUpperCase()
-                if (aTitle > bTitle){
-                    return -1
-                }
-                if (bTitle < aTitle){
-                    return 1
-                }
-                return 0
-            })
-            
-        
-            setSelectedNote(notes[0])
-        }
-        }
-            }, [ascending, sortCriteria])        
-      
-    if (path.indexOf("notes") != -1){
-        if(path[path.length-1]==='notes'){
-            notes=notes
-        }
-        else{
-        noteSelected = notes?.filter(note => note.id === Number(path[path.length-1]))[0]
-        setSelectedNote(noteSelected)
-        }
-    }else if(path.indexOf("notebooks") != -1){
-        notes = notes?.filter(note => note.notebok_id=== Number(path[path.length-1]))[0]
-        setSelectedNote(noteSelected)
+  const noteClick = (note) => {
+    setSelectedNote(note);
+  };
 
+  useEffect(() => {
+    if (notes) {
+      //   setSelectedNote("");
+      if (ascending && sortCriteria === "updatedAt") {
+        notes.sort((a, b) => {
+          return new Date(a.updated_at) - new Date(b.updated_at);
+        });
+      } else if (ascending && sortCriteria === "title") {
+        notes.sort((a, b) => {
+          let aTitle = a.title.toUpperCase();
+          let bTitle = b.title.toUpperCase();
+          if (aTitle < bTitle) {
+            return -1;
+          }
+          if (bTitle < aTitle) {
+            return 1;
+          }
+          return 0;
+        });
+      } else if (!ascending && sortCriteria === "updatedAt") {
+        notes.sort((a, b) => {
+          return new Date(b.updated_at) - new Date(a.updated_at);
+        });
+      } else if (!ascending && sortCriteria === "title") {
+        notes.sort((a, b) => {
+          let aTitle = a.title.toUpperCase();
+          let bTitle = b.title.toUpperCase();
+          if (aTitle > bTitle) {
+            return -1;
+          }
+          if (bTitle < aTitle) {
+            return 1;
+          }
+          return 0;
+        });
+
+        setSelectedNote(notes[0]);
+      }
     }
-    
-    
-    
-return(
+  }, [ascending, sortCriteria]);
+
+  if (path.indexOf("notes") != -1) {
+    if (path[path.length - 1] === "notes") {
+      notes = notes;
+    } else {
+      noteSelected = notes?.filter((note) => note.id === selectedNote?.id)[0];
+      setSelectedNote(noteSelected);
+    }
+  } else if (path.indexOf("notebooks") != -1) {
+    notes = notes?.filter(
+      (note) => (note.notebook_id === note.id) === selectedNote?.id
+    )[0];
+    setSelectedNote(noteSelected);
+  }
+
+  return (
     <div className="Note-Page__container">
-        <div className="notesidebar__container">
+      <div className="notesidebar__container">
         <div className="notesidebar__header">
-            <div className="notesidebar__innerheader">
-                <i className="fas fa-file-alt"></i>
-                <span>Notes</span>
+          <div className="notesidebar__innerheader">
+            <i className="fas fa-file-alt"></i>
+            <span>Notes</span>
+          </div>
+          <div className="notesidebar__innerheader2">
+            <span>{`${notes?.length} notes`}</span>
+            <div className="filter-sort-buttons__container">
+              {sortCriteriaDD ? (
+                <NoteDropDown
+                  items={sortCriteriaList}
+                  sortCriteriaDD={sortCriteriaDD}
+                  setSortCriteriaDD={setSortCriteriaDD}
+                  setSortCriteria={setSortCriteria}
+                />
+              ) : null}
+              {ascending ? (
+                <i
+                  className="fas fa-sort-amount-up"
+                  onClick={() => setAscending(false)}
+                ></i>
+              ) : (
+                <i
+                  className="fas fa-sort-amount-down"
+                  onClick={() => setAscending(true)}
+                ></i>
+              )}
+              <i
+                className="fas fa-filter"
+                onClick={() => setSortCriteriaDD(true)}
+              ></i>
             </div>
-            <div className="notesidebar__innerheader2">
-                <span>{`${notes?.length} notes`}</span>
-                <div className="filter-sort-buttons__container">
-                {sortCriteriaDD?<NoteDropDown items={sortCriteriaList} sortCriteriaDD={sortCriteriaDD} setSortCriteriaDD={setSortCriteriaDD} setSortCriteria={setSortCriteria}/>:null}
-                {ascending? <i className="fas fa-sort-amount-up" onClick={()=>setAscending(false)}></i>:<i className="fas fa-sort-amount-down" onClick={()=>setAscending(true)}></i>}
-                <i className="fas fa-filter" onClick={()=>setSortCriteriaDD(true)}></i>
+          </div>
+        </div>
+        <div>
+          {notes?.map((note) => (
+            <div
+              className="note_side__outer"
+              onClick={() => noteClick(note)}
+              key={note.id}
+            >
+              <NoteInList note={note} />
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="note-page__editor__container">
+        <NoteHeader selectedNote={selectedNote} />
+        {/* {console.log(selectedNote, "FROM NOTEPAGE JS")} */}
+        {selectedNote ? <Note /> : <div>Please select a Note</div>}
 
-            </div>
-            </div>
-            </div>
-            <div>
-                {notes?.map(note =>(
-                    <div onClick={()=>setSelectedNote(note)} key={note.id}><NoteInList note={note} /></div>
-                    ))}
-            </div>
-        
+        {/* <Note /> */}
+      </div>
     </div>
-    <div className="note-page__editor__container">
-        <NoteHeader />
-       
-        {console.log(selectedNote, "FROM NOTEPAGE JS")}
-       { selectedNote? <Note /> : <div>loading...</div>}
-
-    </div>
-    </div>
-    )
-    
-
-}
-export default NotePage
+  );
+};
+export default NotePage;
