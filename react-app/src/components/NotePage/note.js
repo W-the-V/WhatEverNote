c7e94eb2a4df9ad75da7b1317a225ba52d15c5d7
Rheannone <73176069+Rheannone@users.noreply.github.com>
2021-05-01 14:26:55 -0400

    working on autosave

--- react-app/src/components/NotePage/note.js
+++ react-app/src/components/NotePage/note.js
@@ -72,7 +72,7 @@ export const CustomToolbar = () => (
     <span class="ql-formats">
       <button class="ql-link"></button>
       <button class="ql-image"></button>
-      <button class="ql-video"></button>
+      {/* <button class="ql-video"></button> */}
     </span>
   </div>
 );
@@ -90,25 +90,25 @@ const Note = (props) => {
     if (selectedNote && selectedNote.text) {
       setEditorHtml(selectedNote.text);
       setLoaded(true);
-      // console.log("YOU WORKING if selectednote && ");
+ 
     }
     if (editorHtml) {
-      // console.log("YOU WORKING if editorHtml");
+
 
       return;
     }
   }, [selectedNote, setSelectedNote]);
- 
-  function autoSave(e) {
-    console.log(e);
-  }
-  const handleSaveNote = () => {
 
-    let form = document.getElementsByClassName('ql-editor');
+  const handleSaveNote = async () => {
+  let noteTitle = document.getElementById('note-title-input').value
+  let form = document.getElementsByClassName('ql-editor');
   if(form && selectedNote){
     form = form[0].innerHTML
-    let updatedNote = {id:selectedNote.id,user_id:user.id, title:selectedNote.title, notebook_id:selectedNote.notebook_id, text: form}
-    dispatch(editNote(updatedNote))
+    let updatedNote = {id:selectedNote.id,user_id:user.id, title:noteTitle, notebook_id:selectedNote.notebook_id, text: form}
+    let res = await dispatch(editNote(updatedNote))
+    if(res){
+      setSelectedNote(updatedNote)
+    }
     setSaving("all changes saved.")
   }}
   // let quill = new Quill('#editor__container', {
